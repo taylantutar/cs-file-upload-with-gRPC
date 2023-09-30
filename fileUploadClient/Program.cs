1c819580ef21f74a0a82f39b7b849ea611008510
Taylan Tutar <tylntutar@gmail.com>
2023-09-30 18:29:57 +0300

    File upload from client to server

--- fileUploadClient/Program.cs
+++ fileUploadClient/Program.cs
@@ -1,23 +1,47 @@
-﻿using System.Reflection;
-using System.IO;
-
-
+﻿using Grpc.Net.Client;
+using fileServer;
+using Google.Protobuf;
+using System.Text;
 
 Console.WriteLine(Environment.CurrentDirectory);
 
+var channel = GrpcChannel.ForAddress("http://localhost:5253");
+var client = new FileUD.FileUDClient(channel);
 
-var files = Directory.EnumerateFiles(Path.Combine(Environment.CurrentDirectory, "wwwroot"));
+var path = Directory.GetCurrentDirectory();
+var url = Path.Combine(path, "deneme.txt");
 
-foreach (var file in files)
+Console.WriteLine($"URL: {url}");
+
+var fileStream = new FileStream(url, FileMode.Open);
+
+var request = new fileUploadRequest
 {
-    using FileStream fileStream = new FileStream(file, FileMode.Open);
-    byte[] buffer = new byte[2];
-    int readedData = 0;
-    while ((readedData = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-    {
-        Console.WriteLine(readedData);
-    }
+    FileSize = fileStream.Length,
+    Fullname = $"{Path.GetFileNameWithoutExtension(fileStream.Name)}.{Path.GetExtension(fileStream.Name)}",
+    ReadedData = 0
+};
+
+var clientStreamingCall = client.FileUpload();
+
+var buffer = new byte[64];
+
+while ((request.ReadedData = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+{
+    Console.Write(Encoding.Default.GetString(buffer));
+
+    if (request.ReadedData < buffer.Length)
+        Array.Resize(ref buffer, request.ReadedData);
+
+    request.Buffer = ByteString.CopyFrom(buffer);
+
+    await clientStreamingCall.RequestStream.WriteAsync(request);
+
+    Array.Clear(buffer, 0, request.ReadedData);
 }
+
+await clientStreamingCall.RequestStream.CompleteAsync();
+fileStream.Close();
 
 Console.ReadLine();
 
